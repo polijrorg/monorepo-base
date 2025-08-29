1041bccac33cb34b5aeb5213ef93e3397ffdd4a4
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-08-29 15:07:05 -0300

    chore: improve better auth to latest

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [{
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts", "tests/**"]
 }, ...compat.extends("next/core-web-vitals", "next/typescript"), {
   ignores: ["src/generated/**"],
 }];

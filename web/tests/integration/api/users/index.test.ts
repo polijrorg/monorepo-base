ab7cd29d3c25513ef6d384e19f2b5fb559075002
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-09-13 11:09:14 -0300

    fix: update user registration tests and switch to server function

--- web/tests/integration/api/users/index.test.ts
+++ web/tests/integration/api/users/index.test.ts
@@ -2,9 +2,14 @@ import { describe, it, vi, beforeEach, expect, Mock } from "vitest";
 import * as userService from '@/backend/services/users'
 import { postUserMock } from "../../mocks/user";
 import { createRequest } from "../../mocks/requests";
+import { auth } from "@/auth";
 
 vi.mock('@/backend/services/users', () => ({
   findUserByEmail: vi.fn(),
+}))
+
+vi.mock('@/auth', () => ({
+  auth: vi.fn()
 }))
 
 import { POST } from '@/backend/api/users/route'
@@ -14,7 +19,7 @@ describe('POST /api/users', () => {
     vi.clearAllMocks();
   });
 
-  const createUserRequest = () => createRequest(postUserMock, 'user')
+  const createUserRequest = () => createRequest(postUserMock, 'users')
 
   it("should fail if there's another user with the same email", async () => {
     (userService.findUserByEmail as Mock).mockResolvedValue(postUserMock);
@@ -26,6 +31,11 @@ describe('POST /api/users', () => {
   it('should register if everything is alright', async () => {
     (userService.findUserByEmail as Mock).mockResolvedValue(null);
 
+    if (!('api' in auth)) {
+      auth.api = {};
+    }
+    (auth.api.signUpEmail as unknown as Mock) = vi.fn().mockResolvedValue(postUserMock);
+    
     const response = await POST(createUserRequest());
     expect(response?.status).toBe(201);
     

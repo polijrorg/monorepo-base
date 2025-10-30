dfc35759a7dcfc3542ae2a6446351741df098c1c
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-10-30 16:09:54 -0300

    feat: clarify the example initial page

--- web/src/app/(frontend)/(landing-pages)/_components/Embarcar.tsx
+++ web/src/app/(frontend)/(landing-pages)/_components/Embarcar.tsx
@@ -8,7 +8,7 @@ function Embarcar({ isLogged }: { isLogged: boolean }) {
       href={isLogged ? '/aprender' : '/cadastro'}
       className="text-pink-50 text-4xl button-lg border-pink-200 bg-pink-500 colorTransition hover:bg-pink-400"
     >
-      {isLogged ? 'Continue Aprendendo' : 'Embarque agora'}
+      {isLogged ? 'Continue Aprendendo' : 'Cadastro (exemplo)'}
     </Link>
   );
 }

15b699bcd3b89911223410a54ccf7e9f50e35b4c
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-12-14 18:42:35 -0300

    bump next and uncessfully try fixing nativewind dep problem

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}', './screens/**/*.{js,ts,tsx}', './global.css', './contexts/**/*.{js,ts,tsx}', './lib/**/*.{js,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
   theme: {
